4f90eba4ddec28a717fafa2bead0475bf25fd682
Immolazione <federico.deered@gmail.com>
2022-01-10 15:48:42 +0100

    start mail excercise, add array and prompt

--- js/script.js
+++ js/script.js
@@ -4,7 +4,16 @@ console.log("JS OK");
 // Crea un array di email;
 // Chiedi all’utente la sua email.
 // controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
+
 // # Gioco dei dadi
 //  Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Stampare in console i tiri di dado e il risultato.
+
+//MAIL 
+
+emails = ["mail1@gmail.com", "mail2@gmail.com", "mail3@gmail.com", "mail4@gmail.com"];
+console.table(emails);
+
+const currentEmail = prompt("Qual è la tua mail?", "example.mail@gmail.com").trim("");
+console.log(currentEmail);
