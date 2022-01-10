a2191f9eafe45f2666f30c8542c554f3cfaa97ed
Immolazione <federico.deered@gmail.com>
2022-01-10 15:29:00 +0100

    add files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("JS OK");
+
+// # Mail
+// Crea un array di email;
+// Chiedi all’utente la sua email.
+// controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
+// # Gioco dei dadi
+//  Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Stampare in console i tiri di dado e il risultato.
