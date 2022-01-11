351cd0436230f926c4c5c5f3f4c6c1fe0836831b
Immolazione <federico.deered@gmail.com>
2022-01-11 10:38:36 +0100

    separate ex in two js files

--- js/script-dice.js
+++ js/script-dice.js
@@ -1,39 +1,9 @@
 console.log("JS OK");
 
-// # Mail
-// Crea un array di email;
-// Chiedi all’utente la sua email.
-// controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
-
 // # Gioco dei dadi
 //  Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Stampare in console i tiri di dado e il risultato.
-
-//MAIL 
-
-//ARRAY CON LE MAIL STORATE
-const emails = ["mail1@gmail.com", "mail2@gmail.com", "mail3@gmail.com", "mail4@gmail.com"];
-console.table(emails);
-
-//CHIEDERE MAIL ALL'UTENTE
-const currentEmail = prompt("Qual è la tua mail?", "example.mail@gmail.com").trim("");
-console.log(currentEmail);
-
-//ACCOGLIENZA SE MAIL PRESENTE, NOTIFICA SE MAIL ASSENTE
-for (let i = 0; i < emails.length; i++) {
-    if (emails[i] === currentEmail) {
-        console.log("Bentornato!");
-    } else {
-        console.log("Email errata, se è la prima volta procedi con la registrazione, altrimenti ritenta");
-        }
-}
-
-// if (emails.includes(currentEmail)) {
-//     console.log("Bentornato!");
-// } else {
-//     console.log("Email errata, se è la prima volta procedi con la registrazione, altrimenti ritenta");
-// }
 
 //GIOCO DEI DADI
 
---
+++ js/script-email.js
@@ -0,0 +1,32 @@
+console.log("JS OK");
+
+// # Mail
+// Crea un array di email;
+// Chiedi all’utente la sua email.
+// controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
+
+
+//MAIL 
+
+//ARRAY CON LE MAIL STORATE
+const emails = ["mail1@gmail.com", "mail2@gmail.com", "mail3@gmail.com", "mail4@gmail.com"];
+console.table(emails);
+
+//CHIEDERE MAIL ALL'UTENTE
+const currentEmail = prompt("Qual è la tua mail?", "example.mail@gmail.com").trim("");
+console.log(currentEmail);
+
+//ACCOGLIENZA SE MAIL PRESENTE, NOTIFICA SE MAIL ASSENTE
+for (let i = 0; i < emails.length; i++) {
+    if (emails[i] === currentEmail) {
+        console.log("Bentornato!");
+    } else {
+        console.log("Email errata, se è la prima volta procedi con la registrazione, altrimenti ritenta");
+        }
+}
+
+// if (emails.includes(currentEmail)) {
+//     console.log("Bentornato!");
+// } else {
+//     console.log("Email errata, se è la prima volta procedi con la registrazione, altrimenti ritenta");
+// }
